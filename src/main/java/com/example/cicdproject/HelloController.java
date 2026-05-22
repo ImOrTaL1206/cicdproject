bc92737a3f52b3deae7cc8ac5f0dbfc1c87faca4
ImOrTaL1206 <avi1206nash@gmail.com>
2026-05-23 00:01:30 +0530

    Added webpage and image

--- src/main/java/com/example/cicdproject/HelloController.java
+++ src/main/java/com/example/cicdproject/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "CI/CD Project executed Successfully";
+        return "CI/CD Project excute Successfully";
     }
 
     @GetMapping("/health")
