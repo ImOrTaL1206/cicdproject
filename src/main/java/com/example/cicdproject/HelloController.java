efe38e58239c9e28687326435c6fd61f8b42911a
ImOrTaL1206 <avi1206nash@gmail.com>
2026-05-23 09:28:00 +0530

    Enabled webpage

--- src/main/java/com/example/cicdproject/HelloController.java
+++ src/main/java/com/example/cicdproject/HelloController.java
@@ -6,10 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
-    public String home() {
-        return "CI/CD Project excute Successfully";
-    }
 
     @GetMapping("/health")
     public String health() {
